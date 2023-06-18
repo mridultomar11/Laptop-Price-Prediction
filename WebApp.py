1315da547dc6e0c4a4bb40eb88737a71182b56e7
mridultomar11 <123926094+mridultomar11@users.noreply.github.com>
2023-06-18 18:22:16 +0530

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -6,8 +6,8 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 import streamlit as st
 import pickle
